14475eb855d39d828bb9ef3cd496dc8eb05817ae
Jonny Hofmann <jonny.hofmann@divtec.ch>
2017-07-28 10:29:39 +0200

    Rebuild with fix

--- molecule_example/src/Titile.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-class Title extends React.Component {
-    render(
-        <div className="PageTitel">
-    )
-}
---
+++ molecule_example/src/Title.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Title extends React.Component {
+    render(
+        <div className="PageTitel">
+            
+        </div>
+    )
+}
